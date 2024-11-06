75716840cb576e14c005d7f5f317eebbb5b44b3d
ocean_520x <ocean_520x@bytespark.app>
2024-11-06 23:55:22 +0800

    feat: 自动引入组件配置、自动引入方法配置

--- src/views/home.vue
+++ src/views/home.vue
@@ -2,12 +2,14 @@
   <el-button type="primary">sure</el-button>
   <div>
     {{ userStore.name }}
+    {{ test }}
   </div>
 </template>
 
 <script setup lang="ts">
 import user from '@/store/user';
 const userStore = user();
+const test = ref('test');
 </script>
 
 <style lang="scss" scoped></style>
--- viteConfig/autoImport.ts
+++ viteConfig/autoImport.ts
@@ -6,9 +6,19 @@ export default (plugins: Plugin[]) => {
   plugins.push(
     AutoImport({
       resolvers: [ElementPlusResolver()],
+      imports: ['vue', 'vue-router'],
+      dirs: ['src/composables/**/*', 'src/store/**/*'],
+      dts: 'typeDeclaration/autoImport.d.ts',
+      vueTemplate: true,
+      defaultExportByFilename: true
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+      extensions: ['vue', 'tsx'],
+      dirs: ['src/components', 'src/layout', 'src/views'],
+      //同名组件时以文件为前缀，用于区分组件
+      directoryAsNamespace: true,
+      dts: 'typeDeclaration/components.d.ts'
     })
   )
 }
