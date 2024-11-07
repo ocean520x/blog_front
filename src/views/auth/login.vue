478e8e4a9ed414fcdde056ec7033948dffaaa7a7
ocean_520x <ocean_520x@bytespark.app>
2024-11-08 00:40:14 +0800

    feat: 登录静态页面

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    login
+  <div class="bg-login flex h-screen w-full items-center justify-center bg-cover bg-no-repeat">
+    <div class="rounded-xl bg-gray-800 bg-opacity-50 px-16 py-10 shadow-lg backdrop-blur-md max-sm:px-8">
+      <div class="text-white">
+        <div class="mb-8 flex flex-col items-center">
+          <icon-book-open theme="multi-color" size="80" :fill="['#333', '#ff362f', '#FFF', '#43CCF8']" />
+          <h1 class="mb-2 text-2xl">Ocean's Blog</h1>
+          <span class="text-gray-300">Enter Login Details</span>
+        </div>
+      </div>
+      <form action="#">
+        <div class="mb-4 text-lg">
+          <input
+            class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+            type="text" name="name" placeholder="id@email.com" />
+        </div>
+
+        <div class="mb-4 text-lg">
+          <input
+            class="rounded-3xl border-none bg-yellow-400 bg-opacity-50 px-6 py-2 text-center text-inherit placeholder-slate-200 shadow-lg outline-none backdrop-blur-md"
+            type="Password" name="name" placeholder="*********" />
+        </div>
+
+        <div class="mt-8 flex justify-center text-lg text-black">
+          <button type="submit"
+            class="rounded-3xl bg-yellow-400 bg-opacity-50 px-10 py-2 text-white shadow-xl backdrop-blur-md transition-colors  duration-300 hover:bg-yellow-600">Login</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -8,6 +34,4 @@
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'login': "url('/public/images/login.jpg')"
+      }
+    },
   },
   plugins: [],
 }
