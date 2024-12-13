121e0e6bb13b89387ad57e443659e901b74e8702
Ocean <ocean_520x@bytespark.app>
2024-12-13 17:15:46 +0800

    fix: 修复登陆中状态不能跳去登陆、注册、重置密码页面

--- src/router/auth.ts
+++ src/router/auth.ts
@@ -5,7 +5,12 @@ export default {
   component: () => import('@/layout/auth.vue'),
   children: [
     { path: 'login', name: 'login', meta: { guest: true }, component: () => import('@/views/auth/login.vue') },
-    { path: 'repassword', name: 'repassword', component: () => import('@/views/auth/repassword.vue') },
-    { path: 'register', name: 'register', component: () => import('@/views/auth/register.vue') },
+    {
+      path: 'repassword',
+      name: 'repassword',
+      meta: { guest: true },
+      component: () => import('@/views/auth/repassword.vue'),
+    },
+    { path: 'register', name: 'register', meta: { guest: true }, component: () => import('@/views/auth/register.vue') },
   ],
 } as RouteRecordRaw;
