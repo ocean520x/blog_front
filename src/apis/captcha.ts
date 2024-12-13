fad055b202bb81a957bf5488ec8e1e40f2009e31
Ocean <ocean_520x@bytespark.app>
2024-12-13 17:16:40 +0800

    fix: 修复刷新验证码不显示错误提示问题

--- src/apis/captcha.ts
+++ src/apis/captcha.ts
@@ -2,7 +2,10 @@ import myAxios from '@/plugins/axios';
 import { MyCaptcha } from '@/interfaces/apiResponse';
 
 export function getCaptcha() {
-  return myAxios.request<MyCaptcha>({
-    baseURL: '/captcha/api/math',
-  });
+  return myAxios.request<MyCaptcha>(
+    {
+      baseURL: '/captcha/api/math',
+    },
+    { clearValidateError: false },
+  );
 }
