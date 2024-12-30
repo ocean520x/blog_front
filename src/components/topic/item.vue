1a782266f5166354754d722e4ee47c541b20042f
Ocean <ocean_520x@bytespark.app>
2024-12-30 17:56:32 +0800

    feat: 提取单个帖子项组件

--- src/components/topic/item.vue
+++ src/components/topic/item.vue
@@ -12,7 +12,12 @@ const { item } = defineProps<{
   >
     <div class="col-span-10 flex items-center justify-start gap-2">
       <el-avatar shape="square" :size="50" :src="item.user.avatar" />
-      <span>{{ item.title }}--{{ item.category.title }}</span>
+      <router-link
+        :to="{ name: 'per.category', params: { c_id: item.category.id } }"
+        class="text-xs text-cyan-600 hover:text-cyan-500 duration-300 p-1 rounded-sm border border-cyan-600"
+        >{{ item.category.title }}</router-link
+      >
+      <span>{{ item.title }}</span>
     </div>
     <div class="col-span-2 flex items-center justify-center text-sm text-slate-600 gap-1">
       <icon-stopwatch-start theme="outline" size="18" fill="#7ed321" />
