0e693aa0b77553e5b165684513858ab64b2e94ac
ocean <ocean_520x@bytespark.app>
2025-03-12 00:25:42 +0800

    feat（全局）：Element组件国际化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
-
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 </script>
 
 <template>
-  <Suspense>
+  <el-config-provider :locale="zhCn">
+    <Suspense>
     <router-view />
   </Suspense>
+  </el-config-provider>
 </template>
 
 <style lang="scss" scoped>
