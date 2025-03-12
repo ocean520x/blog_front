4b93988a3ac962a6d4a6b5ed64aa758f105fff29
Ocean <ocean_520x@bytespark.app>
2025-03-12 10:33:56 +0800

    feat（后台）：帖子大类列表实现拖拽效果

--- src/views/admin/category/index.vue
+++ src/views/admin/category/index.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import AdminCategoryItem from '@/components/admin/category-item.vue'
 import myTopic from '@/composables/myTopic'
+import draggable from 'vuedraggable'
 
 const { categories, getCategories, delCategory } = myTopic()
 await getCategories()
 </script>
 
 <template>
-  <main v-if="categories" class="grid grid-cols-6 gap-3 p-3">
-    <AdminCategoryItem v-for="(category) in categories" :key="category.id" :category="category" @del-category="delCategory" />
+  <main v-if="categories">
+    <draggable
+      v-model="categories"
+      group="people"
+      item-key="id"
+      class="grid grid-cols-6 gap-3 p-3"
+    >
+      <template #item="{ element: category }">
+        <AdminCategoryItem :category="category" @del-category="delCategory" />
+      </template>
+    </draggable>
   </main>
 </template>
 
